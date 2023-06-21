81d7776b2238389a0d99f7fd2e9267c941c3a4cc
Varrel Enrico <varrelenrico23@gmail.com>
2023-06-21 14:25:52 +0700

    Initial Commit

---
+++ testtttapp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  testtttapp
+//
+//  Created by Oei Varrel Enrico Prayudisti on 21/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, dunia!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testtttapp/testtttappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testtttappApp.swift
+//  testtttapp
+//
+//  Created by Oei Varrel Enrico Prayudisti on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct testtttappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
