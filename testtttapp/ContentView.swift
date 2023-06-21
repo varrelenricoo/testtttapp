86c63399f164f2b66ccfd6e53e8a41017de91acb
Varrel Enrico <varrelenrico23@gmail.com>
2023-06-21 14:29:13 +0700

    bikin list haihaiahi

--- testtttapp/ContentView.swift
+++ testtttapp/ContentView.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, dunia!")
+        List {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, haihaihai!")
+            };VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, haihaihai!")
+            };VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, haihaihai!")
+            }
         }
         .padding()
     }
